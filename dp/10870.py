04e579eca1fa8c1bd1e8a8693cd82f7c90a6b97d
kimhyuncheol <rekaz1@naver.com>
2023-12-28 12:26:56 +0900

    first commit

---
+++ dp/10870.py
@@ -0,0 +1,12 @@
+#백준 10870 피보나치 수 5
+
+import sys
+
+n = int(sys.stdin.readline())
+
+pibo = [i for i in range(n+1)]
+
+for i in range(2, n+1):
+    pibo[i] = pibo[i-1] + pibo[i-2]
+
+print(pibo[n])
