41fdc6913d36bafc27efab2395f4f263e256a771
kimhyuncheol <rekaz1@naver.com>
2024-01-02 13:58:30 +0900

    백준 1463 실3 // i % 2, i % 3, i - 1 나누어 생각

--- dp/17626.py
+++ dp/17626.py
@@ -1,14 +1,19 @@
-#백준 17626 실3 => 110764 KB, 168ms
-
+# 백준 1463 실3 dp 26분 // 39368 KB , 552ms
 import sys
-import math
 
 n = int(sys.stdin.readline())
 
-dp = [ i for i in range(n + 1)]
+dp = [ 0 for _ in range(n+1)]
 
-for i in range(2, n+1):
-    for j in range(1, int(math.sqrt(i)) + 1):
-        dp[i] = min(dp[i-j**2] + 1, dp[i])
+dp[1] = 0
+
+for i in range(2, n + 1):
+    dp[i] = dp[i-1] + 1
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3] + 1)
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2] + 1)
 
 print(dp[n])
+
+# i%3 과 i%2 를 elif 로 작성하면 오류가 난다. 주의. // 또한 %3의 연산을 먼저해야 최소 계산 횟수가 계산되니 순서도 주의
